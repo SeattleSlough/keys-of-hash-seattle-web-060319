529b1023f4f602ad8bef669968b2e3b473015a15
Michael Maggs <michaelmaggs@gmail.com>
2019-05-24 19:00:55 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def keys_of(arguments)
     array = *arguments
-    array1 = *keys_of.animals["Panama"]
+    animals["Panama"]
   end
   
   ## Finds matching values and returns an array of corresponding keys 
