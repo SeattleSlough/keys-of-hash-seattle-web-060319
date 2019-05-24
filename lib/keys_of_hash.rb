ba0cc0d54ab203edac017281e3c13db79b6485a0
Michael Maggs <michaelmaggs@gmail.com>
2019-05-24 19:16:52 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,10 @@
 require 'pry'
 class Hash
   def keys_of(arguments)
+     binding.pry
     .each do |key, value|
     if value == arguments
       *key
-    binding.pry
   end
   
   ## Finds matching values and returns an array of corresponding keys 
